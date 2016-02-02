a53c0b84b24b9f1d81b32fc8bbda058f31274f92
Student <mattray.mbr@gmail.com>
2016-02-02 15:11:52 -0700

    added better styling

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ var app = angular.module('myApp', [])
 	app.controller('inspireController', ['$scope', function($scope){
 
 		console.log('hello')
+		$scope.randomQ = {}
 		$scope.quotes = [
 			{
 				author: "H. Jackson Brown, Jr.",
 				text: "The best preparation for tomorrow is doing your best today.",
+				rating: '',
 			},
 			{
 				author: "Salvador Dali",
 				text: "Intelligence without ambition is a bird without wings.",
+				rating: '',
 			},
 			{
 				author: "Lao Tzu",
 				text: "When you realize nothing is lacking, the whole world belongs to you.",
+				rating: '',
+			},
+			{
+				author: "Dr. Wayne Dyer",
+				text: "Peace is the result of retraining your mind to process life as it is, rather than as you think it should be.",
+				rating: '',
 			},
 		]
 
@@ -30,6 +39,17 @@ var app = angular.module('myApp', [])
 
 		$scope.removeQuote = function(index){
 			$scope.quotes.splice(index, 1)
+		}
+
+		$scope.random = function(event){
+			 var pick = Math.floor(Math.random() * $scope.quotes.length)
+			 $scope.randomQ = $scope.quotes[pick]
+			 $scope.showPopup = true;
+		}
+
+		$scope.closePop = function(){
+			$scope.showPopup = false;
+			randomQ = {}
 		}
 
 
